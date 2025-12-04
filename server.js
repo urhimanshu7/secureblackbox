e12ee65875daf14271fd15d832efdcde968c2c2d
urhimanshu7 <urhimansh7@gmail.com>
2025-12-04 15:13:45 +0530

    server.js

--- server.js
+++ server.js
@@ -328,7 +328,7 @@ app.delete('/delete/:filename', auth, async (req, res) => {
 
 // Serve frontend
 app.use(express.static(path.join(__dirname, '../frontend')));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../frontend/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../frontend/login.html')));
 app.get('/register', (req, res) => res.sendFile(path.join(__dirname, '../frontend/register.html')));
 app.get('/dashboard', (req, res) => res.sendFile(path.join(__dirname, '../frontend/upload.html')));
 
@@ -338,6 +338,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 
 
