38d29803cfc106e2285a897dfaf1b3586a4fe7ce
urhimanshu7 <urhimansh7@gmail.com>
2025-12-05 19:33:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // MongoDB setup
 mongoose.connect(
-  "mongodb+srv://<USERNAME>:<PASSWORD>@<CLUSTERNAME>.mongodb.net/<DBNAME>?retryWrites=true&w=majority",
+  "mongodb+srv://himanshu:<db_password>@admin.msumhke.mongodb.net/?appName=admin",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -362,6 +362,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 
 
